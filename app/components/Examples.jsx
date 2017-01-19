6f8cc34a7aebbc80632c75e6e01dd2cffe5c1d19
Jacob Lower <jake.lower17@gmail.com>
2017-01-19 12:25:31 -0600

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var Examples = (props) => {
   return (
-    <h2>Examples!!</h2>
+    <div>
+      <h2>Examples!!</h2>
+      <p>Welcome to Examples</p>
+    </div>
   );
 };
 
